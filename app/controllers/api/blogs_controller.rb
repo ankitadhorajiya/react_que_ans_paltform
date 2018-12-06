d2308b71e4451b8a889b1bb7880c9706ac059860
Your Name <yourname@yopmail.com>
2018-12-06 18:44:06 +0530

    Accidentaily Added code removed

--- app/controllers/api/blogs_controller.rb
+++ app/controllers/api/blogs_controller.rb
@@ -1,6 +1,5 @@
 class Api::BlogsController < ApplicationController
   def index
-    if params[:]
     render json: Blog.admin_approved.as_json(only: %i(id title content tags like_count dislike_count))
   end
 end
